92b3620eb36a9e9abb2b899d7736af35680519a4
cahalls3 <carsonahalls3@gmail.com>
2022-02-16 23:28:18 -0700

    Everything else

---
+++ Mission7/Models/EFBookstoreRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mission7.Models
+{
+    public class EFBookstoreRepository : IBookstoreRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookstoreRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<Book> Books => context.Books;
+    }
+}
--- Mission7/Startup.cs
+++ Mission7/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Mission7.Models;
 
 namespace Mission7
 {
@@ -26,6 +28,13 @@ namespace Mission7
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<BookstoreContext>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:BooksDBConnection"]);
+            });
+
+            services.AddScoped<IBookstoreRepository, EFBookstoreRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
